1a5759a565e3f495937483fc5437370992e1b054
Giuseppe Carnevale <giuseppe.carnevale@ericsson.com>
2018-09-25 13:01:47 +0200

    Improvements

--- material-intro-screen/src/main/java/agency/tango/materialintroscreen/behaviours/IMessageButton.java
+++
@@ -1,6 +0,0 @@
-package agency.tango.materialintroscreen.behaviours;
-
-/** Controls the message button of the slide */
-public interface IMessageButton {
-
-}
--- material-intro-screen/src/main/java/agency/tango/materialintroscreen/listeners/click/MessageButtonClickListener.java
+++
@@ -1,7 +0,0 @@
-//package agency.tango.materialintroscreen.listeners.click;
-//
-//import android.widget.Button;
-//
-//public interface MessageButtonClickListener {
-//    void onClick(Button messageButton);
-//}
